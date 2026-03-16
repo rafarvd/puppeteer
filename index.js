e8e9990839ab3c92056a1b81737c9c203eb5bc66
rafavd <rafarovd@gmail.com>
2026-03-15 21:23:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const express = require("express");
-const puppeteer = require("puppeteer");
 const { scrape } = require("./scrape");
 const { captureScreenshot } = require("./screenshot");
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 10000;
 
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
+app.get("/", (req, res) => res.send("Hello"));
 
 app.get("/screen", captureScreenshot);
 app.get("/scrape", scrape);
 
 app.listen(PORT, () => {
-  console.log(`Servidor está rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
